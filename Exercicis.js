fee8235b559bdad0fbb71693d7b080db7f4114b0
Aleex55 <94048476+Aleex55@users.noreply.github.com>
2022-05-31 09:55:07 +0200

    Initial commit

---
+++ Exercicis.js
@@ -0,0 +1,80 @@
+//1 
+console.log('Alex Arredondo Rodriguez 18/12/2002');
+
+//2
+var num = 42;
+var frase = "Salchipapa";
+var decimal = 4.20;
+
+console.log(num);
+console.log(frase);
+console.log(decimal);
+
+//3
+console.log(num3);
+console.log(frase2);
+console.log(decimal2);
+
+//si es let peta
+var num3 = 84;
+var frase2 = "Salchipapa2";
+var decimal2 = 8.40;
+
+//4
+var nom4 = 4;
+nom4 = "Alex";
+console.log(nom);
+//no peta perque detecta el canvi de tipus de variable i la fa de tipus string
+
+//5
+//Es el mateix que el 4
+
+//6
+ const bigint = 5.678;
+ var num = 1234;
+ console.log(num + bigint);
+ console.log(num - bigint);
+ console.log(num / bigint);
+ console.log(num * bigint);
+
+//7
+ var hipotenusa =  4;
+ var catets = 4;
+
+ console.log("La hipotenusa del triangle es: " + (catets *catets) + (catets *catets));
+
+//8
+ var num =  9;
+ var str = "Salchipapa";
+
+ console.log(num/str);
+
+
+//9
+ var nom = "Alex Arredondo";
+ var lenght = nom.slice(4,10);
+ console.log(lenght.length);
+
+//10
+ var nom = "Alex Arredondo";
+ var nomModificat = nom.replace('x','');
+
+ console.log(nomModificat);
+
+//11
+ var num1 = undefined;
+ var num2 = null;
+
+ console.log(num1+num2);
+//retorna nAn
+
+//12
+
+ console.log(String(true));
+
+//13
+ console.log(Boolean('weenas'))
+
+//14
+ console.log(Number('7'));
+ 123 !== '123';
